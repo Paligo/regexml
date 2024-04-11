08ba98e348ce20acf7fd351bed592bc019e1ddba
Martijn Faassen <faassen@startifact.com>
2024-04-11 18:53:43 +0200

    Document problems.

--- regexml/tests/test_problems.rs
+++ regexml/tests/test_problems.rs
@@ -1,20 +1,15 @@
-// #[test]
-// fn test_choice_with_postfix() {
-//     let regex = Regex::xpath(r#"(WORDS|WORLD|WORD)+S"#, "");
-//     let regex = regex.unwrap();
-//     assert!(regex.is_match(r#"WORDS"#));
-// }
-
 use regexml::Regex;
 
 #[test]
 fn test_infinite_loop() {
+    // {0,0} is really a no-op, but could lead to an infinite loop
+    // in the FixedGreedy operation. Now we create a noop in such a situation.
     let regex = Regex::xpath(r#"(( a | ( bc ) ) {0,0} )+ xyz"#, "x");
-    // y
     let regex = regex.unwrap();
     assert!(regex.is_match(r#"xyz"#));
 }
 
+// this won't trigger a backtrack, so choice is okay
 #[test]
 fn test_choice_without_postfix() {
     let regex = Regex::xpath(r#"(WORDS|WORLD|WORD)+"#, "");
@@ -22,9 +17,20 @@ fn test_choice_without_postfix() {
     assert!(regex.is_match(r#"WORDS"#));
 }
 
+// This should trigger a backtrack, but it seems to work
 #[test]
 fn test_backtrack_attempt() {
     let regex = Regex::xpath(r#"(WORDS)?(WORD)?S"#, "");
     let regex = regex.unwrap();
     assert!(regex.is_match(r#"WORDS"#));
 }
+
+// This triggers a backtrack (as WORDSS won't match) but doesn't
+// work yet
+
+// #[test]
+// fn test_choice_with_postfix() {
+//     let regex = Regex::xpath(r#"(WORDS|WORLD|WORD)+S"#, "");
+//     let regex = regex.unwrap();
+//     assert!(regex.is_match(r#"WORDS"#));
+// }
